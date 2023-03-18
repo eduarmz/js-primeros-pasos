42388fbe6704a9a5def6b628941e0b6bb25a0508
eduarmz <86635923+eduarmz@users.noreply.github.com>
2023-03-18 13:25:09 -0600

    viendo buenas practicas con variables

---
+++ buenas-practicas.js
@@ -0,0 +1,10 @@
+let nombreEmpleado = "Jose Perez";
+let NombreEmpleado = "Pedro PAramo";
+
+let edadEmpleado = 40;
+
+console.log(nombreEmpleado);
+console.log(NombreEmpleado);
+
+let edadEmpleados = "cuarenta";
+console.log(edadEmpleados);
