ea92d81735dea61ae12ddc7b1d061adc9c6a8c87
eduarmz <86635923+eduarmz@users.noreply.github.com>
2023-03-19 01:33:20 -0600

    lab variables

---
+++ Lab-variables/02.7-varibales-lab.js
@@ -0,0 +1,32 @@
+//LABORATORIO
+//Tiempo Estimado: 15-30 minutos
+//Nivel de Dificultad: Fácil
+//Objetivos: Familiarizar al estudiante con:
+    //variables (es decir, nombrar, declarar, inicializar y modificar su valor)
+//Escenario:
+//Nuestra tarea será crear una lista de contactos. Inicialmente, la lista será bastante simple: solo escribiremos tres personas utilizando los datos que se muestran en la tabla a continuación. En el resto del curso, volverá a este script y lo ampliará sistemáticamente con nuevas funciones, utilizando los elementos de JavaScript recién aprendidos.
+//Nombre 	Teléfono 	Correo
+//Maxwell Wright 	(0191) 719 6495 	Curabitur.egestas.nunc@nonummyac.co.uk
+//Raja Villarreal 	0866 398 2895 	posuere.vulputate@sed.com
+//Helen Richards 	0800 1111 	libero@convallis.edu
+
+//Declara e inicializa las variables donde almacenarás toda la información (nueve variables en total). Muestra en la consola información sobre el primer y último contacto en el formulario: nombre/teléfono/correo.
+
+//Nombres
+let nameMax = "Maxwell Wright";
+let nameeRaja = "Raja Villarreal";
+let nameHelen = "Helen Richards";
+
+//Teléfonos
+let telMax = 01917196495;
+let telRaja = 08663982895;
+let telHelen = 08001111;
+
+//Correos
+let mailMax = "Curabitur.egestas.nunc@nonummyac.co.uk";
+let mailRaja = "posuere.vulputate@sed.com";
+let mailHelen = "libero@convallis.edu";
+
+//info primer y último contacto del formulario
+console.log(nameMax + "/" + telMax + "/" + mailMax);
+console.log(nameHelen + "/" + telHelen + "/" + mailHelen);
