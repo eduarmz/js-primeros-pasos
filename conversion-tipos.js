fc309716ff9a6d52faa853a8fde5baaf5601654a
eduarmz <86635923+eduarmz@users.noreply.github.com>
2023-03-18 20:54:36 -0600

    conversion de tipos de datos

---
+++ conversion-tipos.js
@@ -0,0 +1,24 @@
+//Tipos de datos
+//Alfanumerico
+let nombrePasajero = "Pedro Paramo";
+
+console.log(nombrePasajero);
+
+nombrePasajero = "Edu Paramo";
+console.log(nombrePasajero);
+
+//Numerico
+let valorBoleto = 1650;
+const porcentajetasaEmbarque = 60%;
+var gestionAgencia = 120.55;
+
+
+//Logicos (true,false)
+let boletoActivo = true;
+
+//operaciones aritmeticas
+//suma
+let totalBoletos = valorBoleto + valorBoleto*porcentajetasaEmbarque + gestionAgencia;
+
+//orden de precedencia
+let totalBoletosForma2 = valorBoleto + valorBoleto*porcentajetasaEmbarque + gestionAgencia;
