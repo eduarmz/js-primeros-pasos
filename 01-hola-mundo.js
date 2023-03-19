3ba9fe93a7031335d0b5b4451c8245b9d775edce
eduarmz <86635923+eduarmz@users.noreply.github.com>
2023-03-18 22:55:12 -0600

    Haciendo tareas

--- 01-hola-mundo.js
+++ 01-hola-mundo.js
@@ -1,1 +1,27 @@
 console.log("Hola mundo");
+
+
+//Tarea 1. Emplea console.log para enviar tu nombre a la consola
+console.log("Eduardo Rivas");
+
+//Tarea 2. Muestra tu año de nacimiento
+console.log("1995");
+
+//Tarea 3. Vuelve a intentar mostrar tu año de nacimiento, esta vez pasando la fecha sin las comillas.
+console.log(1995);
+
+//Tarea 4. Podemos pasar varios argumentos a console.log separados por comas.
+//Enviar toda la información como un solo argumento.
+//Enviar la información nombre, apellido, año como argumentos separados.
+console.log("Eduardo", "Rivas", "(1995");
+
+//Tarea 5. Envía la misma información (nombre, apellido, año) a la consola, no una al lado de la otra, sino en líneas consecutivas.
+console.log("Eduardo");
+console.log("Rivas");
+console.log("(1995)");
+
+//Tarea 6. Una cadena se puede concatenar usando el signo +, por ejemplo "abc" + "def" serán tratados como "abcdef". Intenta escribir tu nombre, apellido y año de nacimiento en una línea nuevamente, esta vez no separados por comas, sino por el signo +.
+console.loog("Eduardo" + "Rivas" + "(1995)");
+
+//Tarea 7. Coloca espacios en los lugares apropiados, de modo que cuando se muestre, se obtenga el mismo efecto que en la Tarea 4.
+console.log("Eduardo " + "Rivas " + "(1995)");
